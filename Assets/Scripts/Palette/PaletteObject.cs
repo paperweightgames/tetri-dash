fcc12de909ac8195bbb8f6335b59610c4a53fb25
Justinas Grigas <jgrigas@elam.co.uk>
2020-03-21 04:06:48 +0000

    Version 0.1.5
    - Added palette swapping.

--- Assets/Scripts/Palette/PaletteObject.cs
+++ Assets/Scripts/Palette/PaletteObject.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "Palette Object", menuName = "ScriptableObjects/PaletteObject", order = 0)]
-public class PaletteObject : ScriptableObject
+namespace Palette
 {
-    [SerializeField] private Sprite[] _palettes;
-    [SerializeField] private Material _material;
-    private static readonly int PaletteTex = Shader.PropertyToID("_PaletteTex");
-
-    public void SetPalette(int paletteIndex)
+    [CreateAssetMenu(fileName = "Palette Object", menuName = "ScriptableObjects/PaletteObject", order = 0)]
+    public class PaletteObject : ScriptableObject
     {
-        _material.SetTexture(PaletteTex, _palettes[paletteIndex].texture);
+        [SerializeField] private string _prefsKey;
+        [SerializeField] private Texture2D[] _palettes;
+        [SerializeField] private Material _material;
+        private static readonly int PaletteTex = Shader.PropertyToID("_PaletteTex");
+
+        public void SetPalette(int paletteIndex)
+        {
+            var clampedIndex = Mathf.Clamp(paletteIndex, 0, _palettes.Length - 1);
+            _material.SetTexture(PaletteTex, _palettes[clampedIndex]);
+        }
+
+        public void UpdatePalette()
+        {
+            var paletteIndex = PlayerPrefs.GetInt(_prefsKey, 0);
+            SetPalette(paletteIndex);
+        }
     }
 }
